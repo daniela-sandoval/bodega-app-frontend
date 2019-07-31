1182504930b3eb0a05a50de6e3c907b5b92f7c82
Tess Neau <tessneau@gmail.com>
2019-07-31 16:31:17 -0400

    item

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -12,16 +12,12 @@ class ItemList extends Component {
 
 
   render() {
-    // console.log(this.props.category.items)
-    //   if (this.props.category.items.length !== 0) {
   console.log(this.props.category.items)
   return (
     <div className="item-list">
     {this.generateItems()}
     </div>
-    )
-  }
-
+    )}
 }
 
 export default ItemList;
