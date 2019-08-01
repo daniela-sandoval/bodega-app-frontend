7181b96d61467b0bdd9e0890f03827113d1ff34c
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-08-01 15:14:53 -0400

    real 96%

--- src/components/Bodega.js
+++ src/components/Bodega.js
@@ -101,11 +101,7 @@ class Bodega extends Component {
           })
         })
   }
-<<<<<<< HEAD
 
-=======
-
->>>>>>> 83a7ecdb8d3afcb096f0ff2d26f26d9e2451388f
   render() {
     // console.log(this.state)
 
@@ -115,11 +111,7 @@ class Bodega extends Component {
       <Switch>
         <Route path='/bodega/profile' render={(routerProps) => <Profile router={routerProps} userData={this.state.currentUserInfo} />} />
 
-<<<<<<< HEAD
-        <Route path='/bodega/cart' render={(routerProps) => <Cart deleteCartItem={this.deleteCartItem} router={routerProps} cartItems={this.state.currentCart} />}/>
-=======
         <Route path='/bodega/cart' render={(routerProps) => <Cart deleteCartItem={this.deleteCartItem} payCart={this.payCart} router={routerProps} cartItems={this.state.currentCart} currentTotal={this.state.currentTotal} wallet={this.state.wallet}/>}/>
->>>>>>> 83a7ecdb8d3afcb096f0ff2d26f26d9e2451388f
 
         <Route path='/bodega' render={(routerProps) => <Store makeCartItem={this.makeCartItem} router={routerProps} currentCart={this.state.currentCart} categories={this.state.categories}/>} />
       </Switch>
